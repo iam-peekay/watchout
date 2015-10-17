38c787f0ad382ecf5caf931a7fc98c7c48e74468
iam-peekay <iam.preethi.k@gmail.com>
2015-10-16 19:07:47 -0700

    Detect when a enemy touches you.

--- client/watchout.js
+++ client/watchout.js
@@ -13,7 +13,7 @@
 var w = window.innerWidth;
 var h = window.innerHeight;
 
-var svg = d3.select('body').append('svg');
+var svg = d3.select('.board').append('svg');
 
 svg.attr({height: h, width: w});
 
@@ -24,6 +24,15 @@ var Enemy = function(id, x, y, r) {
   this.y = y;
   this.r = r;
 };
+
+Enemy.prototype.checkBounds = function(){
+  if(this.x >= w - this.r || this.x  <= this.r || this.y >= h - this.r || this.y <= this.r){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 // instantiate player class
 var Player = function(id, x, y, r) {
@@ -36,11 +45,24 @@ var Player = function(id, x, y, r) {
 // player method to detect collisions with enemies
 Player.prototype.detectCollision = function() {
 
+  var collisionExists = false;
+  var that = this;
+  enemiesArray.forEach(function(enemy, index){
+    var xDiff = enemy.x - that.x;
+    var yDiff = enemy.y - that.y;
+    var radiusSum = that.r + enemy.r;
+    var sqrRoot = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    if(sqrRoot < radiusSum){
+      collisionExists = true;
+    }
+  });
+    return collisionExists;
 };
 
 // create enemies array
 var enemiesArray = d3.range(25).map(function(d) {
-  return new Enemy(Math.floor(Math.random() * 100), Math.random() * w, Math.random() * h, 15);
+  return new Enemy(Math.floor(Math.random() * 100), Math.floor(Math.random() * w), Math.floor(Math.random() * h), 15);
+
 });
 
 // create a player
@@ -56,10 +78,11 @@ var enemies = svg.selectAll('.enemy').data(enemiesArray, function(d) { return d.
    .attr('cy', function(d) { return d.y; })
    .attr('r', function(d) { return d.r + 'px'; })
    .style('fill', 'grey')
-   .classed('enemy', true);
+
+
 
 // set player attributes
-var player = svg.selectAll('.player').data(player, function(d) { return d.id; })
+var svgPlayer = svg.selectAll('.player').data(player, function(d) { return d.id; })
    .enter()
    .append('circle')
    .attr('cx', function(d){ return d.x; })
@@ -76,28 +99,34 @@ var drag = d3.behavior.drag();
         .attr('cy', function(d) { return d.y; })
 });
 
-player.call(drag);
-
-
+svgPlayer.call(drag);
 
 
 setInterval(function() {
 
   enemies.transition().duration(1200)
-  .ease('back')
   .attr('cx', function(d){
-    d.x = Math.random() * w;
+    d.x =  w * Math.random() ;
+    if(d.checkBounds()){
+      d.x = w/2;
+    }
     return d.x;
   })
   .attr('cy', function(d){
-    d.y = Math.random() * h;
+    d.y = Math.random() * h ;
+    if(d.checkBounds()){
+      d.y = h/2;
+    }
     return d.y;
   })
 
 },1200)
 
-
-
+setInterval(function(){
+  if(player[0].detectCollision()){
+    console.log("hello");
+  }
+}, 100);
 
 
 
